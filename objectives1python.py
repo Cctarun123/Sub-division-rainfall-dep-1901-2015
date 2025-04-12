190132588580767af167636da9d87b8338ffbddb
Cctarun123 <150460678+Cctarun123@users.noreply.github.com>
2025-04-12 19:11:21 +0530

    objectives1python.py

---
+++ objectives1python.py
@@ -0,0 +1,15 @@
+#1. Analyze Yearly Rainfall Trends
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:\\Users\\cctar\\Downloads\\cleaned_rainfall_data.csv")
+yearly_rainfall = df.groupby("YEAR")["ANNUAL"].mean()
+
+plt.figure(figsize=(12, 6))
+plt.plot(yearly_rainfall.index, yearly_rainfall.values, color='blue', linewidth=2)
+plt.title("Average Annual Rainfall Over the Years")
+plt.xlabel("Year")
+plt.ylabel("Average Annual Rainfall (mm)")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
