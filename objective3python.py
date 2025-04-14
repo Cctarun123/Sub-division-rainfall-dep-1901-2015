cf798ad2e3d8130e644383590555ffe6fb3e520f
Cctarun123 <150460678+Cctarun123@users.noreply.github.com>
2025-04-14 21:28:51 +0530

    Update objective3python.py

--- objective3python.py
+++ objective3python.py
@@ -1,7 +1,6 @@
 #3. Compare Rainfall Across Seasons
 seasonal_columns = ['JF', 'MAM', 'JJAS', 'OND']
 seasonal_means = df[seasonal_columns].mean()
-
 plt.figure(figsize=(8, 6))
 plt.bar(seasonal_means.index, seasonal_means.values, color='skyblue')
 plt.title("Average Rainfall Across Seasons")
